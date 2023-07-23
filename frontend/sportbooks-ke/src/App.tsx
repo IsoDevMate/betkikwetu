33aa5d12a86d213447e51a885c0ae5afbfa7a856
IsoDevMate <oumaoduor5827@gmail.com>
2023-07-23 12:08:19 -0700

    implementing the UI

---
+++ frontend/sportbooks-ke/src/App.tsx
@@ -0,0 +1,18 @@
+
+import './App.css';
+import ButtonComponent from './components/button';
+import FloatInput from './componens/input';
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonComponent />
+      <ButtonComponent />
+      <ButtonComponent />
+      <br />
+      <FloatInput />
+    </div>
+  );
+}
+
+export default App;
