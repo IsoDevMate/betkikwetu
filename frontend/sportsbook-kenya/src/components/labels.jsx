142d7abe4da84f412b3e8d8245e51f87c15b7f9f
IsoDevMate <oumaoduor5827@gmail.com>
2023-08-21 11:29:03 +0300

    added label.jsx

---
+++ frontend/sportsbook-kenya/src/components/labels.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Labels = () => {
+  return (                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+    <div>labels</div>
+  )
+}
+
+export default Labels
